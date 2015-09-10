e122847ff58952256dea4b5f11175e7d98f97a52
Radoslav Nikolov <radoslavmnikolov@gmail.com>
2015-09-10 12:13:32 +0300

    Add some properties to appointment view Models/ViewModels/CustomerAppointmentViewModel.cs(IsApproved, IsCanceled,IsExpired)

--- EscortServiceHouseTest/EscortHouseService.Services/Models/ViewModels/CustomerAppointmentViewModel.cs
+++ EscortServiceHouseTest/EscortHouseService.Services/Models/ViewModels/CustomerAppointmentViewModel.cs
@@ -4,15 +4,25 @@
     using EscortService.Models;
 
     public class CustomerAppointmentViewModel
-    {
+    {      
+
         public CustomerAppointmentViewModel(Appointment appointment)
         {
+            this.Id = appointment.Id;
             this.StartTime = appointment.StartTime;
             this.EndTime = appointment.EndTime;
             this.Price = appointment.Price;
             this.Location = appointment.Location;
             this.EscortName = appointment.Escort.UserName;
+            this.IsCanceled = appointment.IsCanceled;
+            if (appointment.IsApproved != null)
+            {
+                this.IsApproved = (bool) appointment.IsApproved;
+            }
+            this.IsExpired = DateTime.Now > appointment.EndTime;
         }
+
+        public int Id { get; set; }
 
         public DateTime StartTime { get; set; }
 
@@ -22,6 +32,12 @@
 
         public string Location { get; set; }
 
-        public string EscortName { get; set; } 
+        public string EscortName { get; set; }
+
+        public bool IsCanceled { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public bool IsExpired { get; set; }
     }
 }
